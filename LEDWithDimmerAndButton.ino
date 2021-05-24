cf9b867bc8b6d9d5d9504ba2de744cd7a4970167
Tom-AW <45534386+Tom-AW@users.noreply.github.com>
2021-05-24 18:28:12 +0100

    button press detect change

--- LEDWithDimmerAndButton.ino
+++ LEDWithDimmerAndButton.ino
@@ -1,29 +1,29 @@
 #include <Adafruit_NeoPixel.h>
 #define LEDPIN 3
 #define BUTTONPIN 4
-#define POTENTPIN A0
-int buttonState = 0;
+#define POTPIN A0
+int stateButton = 0;
 int lastButtonState = 0;
 int lights[3] = {0, 0, 0};
 int i = 0;
 int pot = 0;
-int pMax = 255;
-int pMin = 0;
+int potMax = 255;
+int potMin = 0;
 
-// How many pixels, and which pin to use to send signals.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(10, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  Serial.begin(115200);
-  pixels.begin(); // This initializes the NeoPixel library.
-  pinMode(BUTTONPIN, INPUT);
+  Serial.begin(115200); //115200 baud
+  pixels.begin(); // Initialize NeoPixel library.
+  pinMode(BUTTONPIN, INPUT);  //set button as input on designated pin
 }
 
 void loop()
 {
   delay(2);
-  pot = map(analogRead(POTENTPIN), 0, 1023, pMin, pMax);
+  pot = analogRead(POTPIN); //read pot value
+  pot = map(pot, 0, 1023, potMin, potMax);
   lights[i] = pot;
   pixels.setPixelColor(0, pixels.Color(lights[0], lights[1], lights[2]));
   pixels.show();
@@ -31,13 +31,15 @@ void loop()
   if(digitalRead(BUTTONPIN)==HIGH)
   {
     delay(10);  //10ms wait for swtich debounce
-    i++; //increment RGB position
-    Serial.println(i); //debug print
     
-    if(i==3)  //reset RGB position
+    if(i<2)
     {
-      i=0;
+      i++;  //increment RGB position
+    }else{
+      i=0; //reset RGB position
     }
+    
+    Serial.println(i);  //debug print
     while(digitalRead(BUTTONPIN)==HIGH);  //stay in whilst button held
   }
 }
